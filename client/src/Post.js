81f92ad21cae833b607129ed7a584803b8e38aec
cradle0fFilth <64477775+cradle0fFilth@users.noreply.github.com>
2023-03-17 13:57:30 +0800

    Add pages of blog

---
+++ client/src/Post.js
@@ -0,0 +1,16 @@
+export default function Post(){
+    return (     
+ <div className='post'>
+    <div className='image'>
+      <img src="https://img.huffingtonpost.com/asset/6408bd2b25000059000bc077.jpeg?cache=NPeWh6BkGE&ops=scalefit_720_noupscale"></img>
+    </div>
+    <div className='texts'>
+      <h2>chatGPT-4 will change the world</h2>
+       <p className="info">
+         <a className="author">cradleOffilth </a>
+         <time>2023-01-06 16:45</time>
+       </p>
+      <p className="summary">ChatGPT-4 has the potential to revolutionize the world with its advanced natural language processing capabilities. As a language model that surpasses its predecessor, GPT-3, ChatGPT-4 will be able to generate even more coherent and intelligent responses to a wider range of questions and tasks.</p>
+    </div>
+  </div>)
+}
